902c2f6ba8fd9e82726e9beeee7e3cba430a216f
satanic <gosha.shcherbakov.2016@gmail.com>
2026-01-17 19:52:53 +0300

    Update WeaponOutputHere.lua

--- WeaponOutputHere.lua
+++ WeaponOutputHere.lua
@@ -16,6 +16,7 @@ Hooks:PostHook( WeaponTweakData, "init", "WeaponOutputHere", function(self)
 --self.*internal weapon name*.timers.unequip =                       *time in seconds to unequip weapon*
 ----------------------------------------------------------------------------------------------------------------------------------------
 -------------------example on izhma 12g------------------      <------this blank line
+self.saiga.AMMO_PICKUP = {6, 12}
 self.saiga.stats.damage = 100
 self.saiga.stats.spread = 35
 self.saiga.stats.recoil = 100
@@ -23,5 +24,6 @@ self.saiga.stats.concealment = 30
 self.saiga.timers.reload_not_empty = 1
 self.saiga.timers.reload_empty = 1.5
 ---each individual weapon is separated by a blank line---      <------and this     
+
 
 end )
